e99befbdaa9f67a75f20383790b737f6e557ae4a
Thewischmoop <115495657+Thewischmoop@users.noreply.github.com>
2025-01-11 12:26:17 +0100

    Deleted some files

--- kys/scripts/cases/00_tutorial/scenes/tuto_office/main_scripts.gd.gd
+++ kys/scripts/cases/00_tutorial/scenes/tuto_office/main_scripts.gd.gd
@@ -13,7 +13,16 @@ func _on_pc_pressed() -> void:
 	get_tree().change_scene_to_file("res://assets/cases/00_tutorial/scenes/tuto_office/background/tuto_office_closeup_pc.tscn")
 	
 func _on_board_pressed() -> void:
-	get_tree().change_scene_to_file("res://assets/cases/00_tutorial/scenes/tuto_office/background/tuto_office_board.tscn")
+	get_tree().change_scene_to_file("res://assets/cases/00_tutorial/scenes/tuto_office/background/closeup_whiteboard.tscn")
 
 func _on_certificate_pressed() -> void:
 	get_tree().change_scene_to_file("res://closeup_certificate.tscn")
+
+
+
+func _on_register_pressed() -> void:
+	get_tree().change_scene_to_file("res://assets/cases/00_tutorial/scenes/tuto_office/background/closeup_register.tscn")
+
+
+func _on_window_pressed() -> void:
+	get_tree().change_scene_to_file("res://assets/cases/00_tutorial/scenes/tuto_office/background/closeup_window.tscn")
--- kys/scripts/cases/00_tutorial/scenes/tuto_office/object_scripts/register.gd
+++ kys/scripts/cases/00_tutorial/scenes/tuto_office/object_scripts/register.gd
@@ -9,6 +9,3 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-func _on_pressed() -> void:
-	get_tree().change_scene_to_file("res://scripts/cases/00_tutorial/scenes/tuto_office/00_tuto_office.tscn")
