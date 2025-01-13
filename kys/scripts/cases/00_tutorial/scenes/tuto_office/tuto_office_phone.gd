733d14f19b42541b6c6bb4ebaa94581e32cbe290
croc001 <croc12357@gmail.com>
2025-01-13 18:01:54 +0100

    del aseprite addon and added dialogue

---
+++ kys/scripts/cases/00_tutorial/scenes/tuto_office/tuto_office_phone.gd
@@ -0,0 +1,17 @@
+extends Button
+var call_res = load("res://assets/cases/00_tutorial/scenes/tuto_office/dialogue/phone_call.dialogue")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	self.disabled = true
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_pressed() -> void:
+	$"../phone_ring".free()
+	DialogueManager.show_dialogue_balloon(call_res, "phone_call")
+	print("telefon gedrückt")
