82e4c175a5125ca9e554e995d099d6800e3753ff
Agon69 <canolli.a@hotmail.com>
2025-05-14 23:49:53 +0200

    test

--- kys/assets/global/ui/phone/inventory/InventoryMenu.gd
+++ kys/assets/global/ui/phone/inventory/InventoryMenu.gd
@@ -1,13 +1,10 @@
 extends CanvasLayer
-# oder extends Control, je nachdem, was dein Inventar-Root ist
 
 func _ready():
 	hide()
-	# Wichtig: InventoryMenu soll auch bei Pause Eingaben verarbeiten!
 	process_mode = Node.PROCESS_MODE_ALWAYS
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("inventory_menu"):
 		visible = not visible
-		# Pausiere das Spiel, wenn Inventar offen ist – ansonsten weiterlaufen lassen.
 		get_tree().paused = visible
