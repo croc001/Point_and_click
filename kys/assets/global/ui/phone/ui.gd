edebd8ade1850c685eabf39265d87f0df42a2c7c
Thewischmoop <daniel.pyralov@web.de>
2025-05-19 19:53:13 +0200

    Added Phone UI
    Phone is now in the game please use it for all menu things (MAP, Inventory...)
    the phone is autoloaded in EVERY scene but it is invisble (Dont asked how i did it i have no clue) if you want it to be visible in your scene you need this line of code in the func _ready():
    DetectivePhone.visible = true
    exactly typed like shown or else it wont work

---
+++ kys/assets/global/ui/phone/ui.gd
@@ -0,0 +1,72 @@
+extends CanvasLayer
+
+var menu_open := false
+var is_animating := false
+var phone_start_pos := Vector2()
+var phone_offset_y := -135
+
+func _ready():
+	visible = true #das macht das das gesamte handy angezeigt wird oder nicht
+	phone_start_pos = $Control/PhonePanel.position
+	$Control/PhonePanel/MAP_APP.pressed.connect(_on_map_app_pressed)
+	$Control/PhonePanel/EVIDENCE_APP.pressed.connect(_on_evidence_app_pressed)
+	$Control/PhonePanel/INVENTORY_APP.pressed.connect(_on_inventory_app_pressed)
+	$Control/PhonePanel/QUESTLOG_APP.pressed.connect(_on_questlog_app_pressed)
+	#von hier bis
+func _on_map_app_pressed():
+	print("Map geöffnet")
+
+func _on_evidence_app_pressed():
+	print("Evidenceboard geöffnet")
+
+func _on_inventory_app_pressed():
+	print("Inventory geöffnet")
+
+func _on_questlog_app_pressed():
+	print("Questlog geöffnet")
+	#hier ist nur zum testen ob die buttons funktionieren
+
+func _open_phone():
+	if is_animating or menu_open:
+		return
+	is_animating = true
+	var tween = get_tree().create_tween()
+	var panel = $Control/PhonePanel
+	tween.tween_property(panel, "position", phone_start_pos + Vector2(0, phone_offset_y), 0.4)
+	tween.tween_callback(Callable(self, "_on_tween_opened"))
+
+	# APPS sichtbar machen
+	panel.get_node("MAP_APP").visible = true
+	panel.get_node("EVIDENCE_APP").visible = true
+	panel.get_node("INVENTORY_APP").visible = true
+	panel.get_node("QUESTLOG_APP").visible = true
+
+func _close_phone():
+	if is_animating or not menu_open:
+		return
+	is_animating = true
+	var tween = get_tree().create_tween()
+	var panel = $Control/PhonePanel
+	tween.tween_property(panel, "position", phone_start_pos, 0.4)
+	tween.tween_callback(Callable(self, "_on_tween_closed"))
+
+	# APPS unsichtbar machen
+	panel.get_node("MAP_APP").visible = false
+	panel.get_node("EVIDENCE_APP").visible = false
+	panel.get_node("INVENTORY_APP").visible = false
+	panel.get_node("QUESTLOG_APP").visible = false
+
+func _on_tween_opened():
+	menu_open = true
+	is_animating = false
+
+func _on_tween_closed():
+	menu_open = false
+	is_animating = false
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.pressed:
+		if event.button_index == MOUSE_BUTTON_WHEEL_UP and not menu_open:
+			_open_phone()
+		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN and menu_open:
+			_close_phone()
