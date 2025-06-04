cf2219eee2069616042b2b57fee22057824bcffc
420Pierre <127946382+420Pierre@users.noreply.github.com>
2025-06-05 01:04:55 +0200

    Changed Inventory and Item System
    Changed the inventory and item system. It doesn't completely work as intended yet.

--- kys/assets/global/ui/phone/inventory/InventoryMenu.gd
+++ kys/assets/global/ui/phone/inventory/InventoryMenu.gd
@@ -1,10 +1,28 @@
 extends CanvasLayer
 
 func _ready():
-	hide()
-	process_mode = Node.PROCESS_MODE_ALWAYS
+	inventory.close()
+	#hide()
+	#process_mode = Node.PROCESS_MODE_ALWAYS
 
-func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("inventory_menu"):
-		visible = not visible
-		get_tree().paused = visible
+#func _unhandled_input(event: InputEvent) -> void:
+	#if event.is_action_pressed("inventory_menu"):
+		#visible = not visible
+		#get_tree().paused = visible
+
+@onready var inventory = $Inventory2
+
+func _input(event):
+	if event.is_action_pressed("toggle_inventory"):
+		if inventory.isOpen:
+			inventory.close()
+		else:
+			inventory.open()
+
+
+func _on_inventory_2_closed() -> void:
+	get_tree().paused = false
+
+
+func _on_inventory_2_opened() -> void:
+	get_tree().paused = true
---
+++ kys/assets/global/ui/phone/inventory/items/test_item.gd
@@ -0,0 +1,4 @@
+extends "res://assets/global/ui/phone/inventory/items/item_pick_up.gd"
+
+func collect(inventory: Inventory):
+	super(inventory)
