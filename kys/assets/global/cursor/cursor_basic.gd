059c16c807ba16dccac6fb31c53d1d07b0d39d7b
Thewischmoop <daniel.pyralov@web.de>
2025-06-25 18:20:01 +0200

    update to Cursor

---
+++ kys/assets/global/cursor/cursor_basic.gd
@@ -0,0 +1,9 @@
+extends Sprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
+	
+func _physics_process(delta: float) -> void:
+	global_position = lerp(global_position, get_global_mouse_position(), 16.5 * delta)
