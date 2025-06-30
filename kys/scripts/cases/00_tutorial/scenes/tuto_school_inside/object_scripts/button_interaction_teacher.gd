9e0b63ecd7663403b795c78f3721f883a991b039
croc001 <111568288+croc001@users.noreply.github.com>
2025-06-30 21:26:55 +0200

    BUG: teacher barks doesent show -fixed

--- kys/scripts/cases/00_tutorial/scenes/tuto_school_inside/object_scripts/button_interaction_teacher.gd
+++ kys/scripts/cases/00_tutorial/scenes/tuto_school_inside/object_scripts/button_interaction_teacher.gd
@@ -3,7 +3,8 @@ extends Interactable_object_button
 var dialogue = load("res://assets/cases/00_tutorial/scenes/tuto_school_inside/dialogue/teacher_talk.dialogue")
 
 func _pressed() -> void:
+	if GlobalVar.teacher_dialogue_is_over == true:
+		DialogueManager.show_dialogue_balloon(dialogue, "teacher_barks")
 	if GlobalVar.teacher_dialogue_is_over == false:
 		DialogueManager.show_dialogue_balloon(dialogue, "teacher_talk")
 		GlobalVar.teacher_dialogue_is_over = true
-	
