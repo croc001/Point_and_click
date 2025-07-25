8574e97b843625013be05c580359b227c9b149f4
croc001 <111568288+croc001@users.noreply.github.com>
2025-07-25 14:35:19 +0200

    added dialog for cabin_inside

--- kys/scripts/cases/00_tutorial/scenes/tuto_forest_cabin_inside/tuto_forest_cabin_inside.gd
+++ kys/scripts/cases/00_tutorial/scenes/tuto_forest_cabin_inside/tuto_forest_cabin_inside.gd
@@ -1,9 +1,10 @@
 extends Node2D
 
+var call_res = load("res://assets/cases/00_tutorial/scenes/tuto_forest_cabin_inside/dialogue/cabin_inside_dialogue.dialogue")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	DialogueManager.show_dialogue_balloon(call_res, "inicial_entering")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
